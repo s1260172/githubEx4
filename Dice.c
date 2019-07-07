bde67436f4689731564118d17fdfec43dd2400bd
Sora Okada <sora@SoranoMacBook-ea.local>
2019-07-07 18:37:26 +0900

    ver1

---
+++ Dice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+int main(void) {
+    int sum=0;
+    srand((unsigned int)time(NULL)); // 現在時刻の情報で初期化
+    printf("Rolling the dice...\n");
+    for(int i = 0; i < 2; i++) {
+        // 1から6までの乱数を発生
+        int num = rand() % 6 + 1;
+        sum=sum+num;
+        printf("Die %d: %d\n",i+1,num);
+        
+    }
+    printf("Total value: %d\n",sum);
+    
+    return 0;
+}
